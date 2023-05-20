5581e50fbd423f64bbb50ead552adbec863b13e3
Gabriel Alves <gabrielalves@outlook.com>
2023-05-20 00:17:36 -0300

    feat: removes support for multiple monitors

--- i3/.config/i3/configure_screens.sh
+++ i3/.config/i3/configure_screens.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-all_monitors=($(xrandr | grep "^[^ ]" | tail --lines 2 | grep -oE "^[^ ]* "))
-monitor_1=${all_monitors[0]}
-monitor_2=${all_monitors[1]}
-
-xrandr --output $monitor_1 --mode 1366x768 --pos 1920x0 --rotate normal
-xrandr --output $monitor_2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+~/.screenlayout/hdmi.sh
 polybar main
