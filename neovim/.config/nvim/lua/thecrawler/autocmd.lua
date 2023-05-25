9c2419a45e08783fd2c281a888ba7655cb984b3b
Gabriel Alves <gabrielalves@outlook.com>
2023-05-25 14:41:02 -0300

    feat: change neovim font size

--- neovim/.config/nvim/lua/thecrawler/autocmd.lua
+++ neovim/.config/nvim/lua/thecrawler/autocmd.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
   augroup MyAutocmds
     autocmd!
-    autocmd VimEnter * lua vim.fn.system("alacritty msg config window.padding.x=0 window.padding.y=0 font.size=14")
-    autocmd VimLeave * lua vim.fn.system("alacritty msg config window.padding.x=10 window.padding.y=10 font.size=12")
+    autocmd VimEnter * lua vim.fn.system("alacritty msg config window.padding.x=0 window.padding.y=0")
+    autocmd VimLeave * lua vim.fn.system("alacritty msg config window.padding.x=10 window.padding.y=10")
   augroup END
 ]]
