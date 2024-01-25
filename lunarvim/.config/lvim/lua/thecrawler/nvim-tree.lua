7d69d73ec7b5363c2288b6258fb252c65ca24fd6
Gabriel Alves <gabrielalves@outlook.com>
2024-01-25 09:18:12 -0300

    fix: fix the directory of projects in nvimtree

--- lunarvim/.config/lvim/lua/thecrawler/nvim-tree.lua
+++ lunarvim/.config/lvim/lua/thecrawler/nvim-tree.lua
@@ -11,6 +11,12 @@ end
 lvim.builtin.nvimtree.setup.on_attach = my_on_attach
 lvim.builtin.nvimtree.setup.update_cwd = true
 lvim.builtin.nvimtree.setup.view.width = 40
+lvim.builtin.nvimtree.setup.sync_root_with_cwd = true
+lvim.builtin.nvimtree.setup.respect_buf_cwd = true
+lvim.builtin.nvimtree.setup.update_focused_file = {
+  enable = true,
+  update_root = true
+}
 lvim.builtin.nvimtree.setup.hijack_directories = {
   enable = true,
   auto_open = true,
