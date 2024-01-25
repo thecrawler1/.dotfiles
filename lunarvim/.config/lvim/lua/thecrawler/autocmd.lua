4cf3d7b93bddb64cd5a99d91aed543159a3830d6
Gabriel Alves <gabrielalves@outlook.com>
2024-01-25 10:37:41 -0300

    fix: sets the width of nvimtree when opening a buffer

--- lunarvim/.config/lvim/lua/thecrawler/autocmd.lua
+++ lunarvim/.config/lvim/lua/thecrawler/autocmd.lua
@@ -5,11 +5,23 @@ lvim.autocommands = {
       pattern = { "*" },
       command = "lua vim.fn.system('alacritty msg config window.padding.x=0 window.padding.y=0')"
     }
-  }, {
+  },
+  {
     "VimLeave",
     {
       pattern = { "*" },
       command = "lua vim.fn.system('alacritty msg config window.padding.x=10 window.padding.y=10')"
+    }
+  },
+  {
+    "BufEnter",
+    {
+      pattern = { "*" },
+      callback = function()
+        if pcall(require, 'nvim-tree') then
+          vim.cmd(":NvimTreeResize 40")
+        end
+      end
     }
   }
 }
